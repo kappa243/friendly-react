0c0be043a1657b51e837a82647c8199e3ea575a0
Szymon Idec <szymon.idec@protonmail.com>
2024-10-15 01:08:51 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  env: {
+    es2024: true,
+    node: true
+  },
+  extends: [
+    "expo",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  root: true,
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react/no-unescaped-entities": 0,
+    "indent": ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"]
+  }
+};
