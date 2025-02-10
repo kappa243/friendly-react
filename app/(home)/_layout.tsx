91d62b16c3753407ff881a8566a38169d7e28294
AndrzejStarzyk <astarzyk@student.agh.edu.pl>
2025-02-10 12:50:57 +0100

    Fixed location functionality, will now turn on while user is browsing main screens and therefore is logged in

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -2,10 +2,16 @@ import { HelloWave } from "@/components/HelloWave";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { TText } from "@/components/theme/TText";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import {defaultLocation, LocationContext} from "@/components/LocationProvider";
 import { Tabs } from "expo-router";
 import { View } from "react-native";
+import {useEffect, useState} from "react";
+import {requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject} from "expo-location";
+import {setUserLocation, useUserData} from "@/logic/userData";
+import {useUserStore} from "@/logic/userStore";
 
 function HomeHeader() {
+  
   return (
     <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
       <TText style={{ fontSize: 20, fontWeight: "bold" }}>Friendly</TText>
@@ -15,60 +21,97 @@ function HomeHeader() {
 }
 
 export default function Home() {
-
   const tint = useThemeColor({}, "tint");
 
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  
+  const [location, setLocation] = useState<LocationObject>(defaultLocation);
+  const userData = useUserData();
+  const { user, setUser } = useUserStore();
+  // get user location
+  useEffect(() => {
+    if(userData?.location) {
+      setLocation(userData!.location);
+    }
+
+    async function getCurrentLocation() {
+
+      const { status } = await requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      const curr_location = await getCurrentPositionAsync({});
+
+      if(user && curr_location.coords.longitude !== location.coords.longitude && curr_location.coords.latitude !== location.coords.latitude){
+
+        setLocation(curr_location);
+        await setUserLocation(curr_location);
+      }
+    }
+
+    const interval = setInterval(() => {
+      getCurrentLocation();
+    }, 500000);
+    return () => {
+      clearInterval(interval);
+
+    };
+  }, [location, user, userData]);
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: tint,
-        headerShown: true,
-      }}
-      backBehavior="initialRoute"
-    >
-      <Tabs.Screen name="settings" options={{
-        title: "Settings",
-        tabBarIcon: (({ focused, color }) => (
-          <TabBarIcon name={focused ? "settings" : "settings-outline"} color={color} />
-        ))
-      }} />
-      <Tabs.Screen name="index" options={{
-        headerTitleAlign: "center",
-        headerTitle: () => <HomeHeader />,
-        tabBarIcon: (({ focused, color }) => (
-          <TabBarIcon name={focused ? "home" : "home-outline"} color={color} />
-        ))
-      }} />
-      <Tabs.Screen
-        name="profile/[id]"
-        initialParams={{ id: "" }}
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ focused, color }) => (
-            <TabBarIcon name={focused ? "person" : "person-outline"} color={color} />
-          ),
+    <LocationContext.Provider value={location}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: tint,
+          headerShown: true,
         }}
-      />
-      <Tabs.Screen name="edit_profile" options={{
-        title: "Edit Profile",
-        tabBarIcon: (({ focused, color }) => (
-          <TabBarIcon name={focused ? "person" : "person-outline"} color={color} />
-        ))
-      }} />
-      <Tabs.Screen name="map" options={{
-        title: "Map",
-        tabBarIcon: (({ focused, color }) => (
-          <TabBarIcon name={focused ? "location" : "location-outline"} color={color} />
-        ))
-      }} />
-
-      {/* <Tabs.Screen name="motion" options={{
-        title: "Motion test",
-        tabBarIcon: (({ focused, color }) => (
-          <TabBarIcon name={focused ? "settings" : "settings-outline"} color={color} />
-        ))
-      }}/> */}
-
-    </Tabs>
+        backBehavior="initialRoute"
+      >
+        <Tabs.Screen name="settings" options={{
+          title: "Settings",
+          tabBarIcon: (({ focused, color }) => (
+            <TabBarIcon name={focused ? "settings" : "settings-outline"} color={color} />
+          ))
+        }} />
+        <Tabs.Screen name="index" options={{
+          headerTitleAlign: "center",
+          headerTitle: () => <HomeHeader />,
+          tabBarIcon: (({ focused, color }) => (
+            <TabBarIcon name={focused ? "home" : "home-outline"} color={color} />
+          ))
+        }} />
+        <Tabs.Screen
+          name="profile/[id]"
+          initialParams={{ id: "" }}
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ focused, color }) => (
+              <TabBarIcon name={focused ? "person" : "person-outline"} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen name="edit_profile" options={{
+          title: "Edit Profile",
+          tabBarIcon: (({ focused, color }) => (
+            <TabBarIcon name={focused ? "person" : "person-outline"} color={color} />
+          ))
+        }} />
+        <Tabs.Screen name="map" options={{
+          title: "Map",
+          tabBarIcon: (({ focused, color }) => (
+            <TabBarIcon name={focused ? "location" : "location-outline"} color={color} />
+          ))
+        }} />
+    
+        {/* <Tabs.Screen name="motion" options={{
+            title: "Motion test",
+            tabBarIcon: (({ focused, color }) => (
+              <TabBarIcon name={focused ? "settings" : "settings-outline"} color={color} />
+            ))
+          }}/> */}
+    
+      </Tabs>
+    </LocationContext.Provider>  
   );
 }
